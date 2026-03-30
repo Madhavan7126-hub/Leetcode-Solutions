20ef20e6393c5027a6741804668a5b77319f327d
Madhavan Arumugam <madhavanarumugam1111@gmail.com>
2026-03-30 10:52:29 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (72.18%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,18 +1,34 @@
 class Solution {
     public int[] plusOne(int[] digits) {
+         int len = digits.length ;
 
-        for (int i = digits.length - 1; i >= 0; i--) {
+         if(digits[len-1]!=9)
+         {
+              digits[len-1]++;  //  if the last digit is not 9 then there is no need of extra indices 
+            return digits ;
+         }
+         
+       int ind =0 ;
+       for(int i =len-1 ;i>=0;i--)
+       {
+             if(digits[i]==9)
+             {
+                digits[i]=0 ;
+             }
+             else{
+                 ind =i ;
+                 break ;
+             }
+       }
 
-            if (digits[i] < 9) {
-                digits[i]++;
-                return digits;
-            }
+       if(ind==0 && digits[ind]==0)
+       {
+           int[]  ans = new int[len+1];
+           ans[0]=1 ;
+           return ans ;
+       }
+       digits[ind]++;
+        return digits ;
 
-            digits[i] = 0;
-        }
-
-        int[] result = new int[digits.length + 1];
-        result[0] = 1;
-        return result;
     }
 }
