f25fc14ecf0501c6c540875ee2e2aff10e493913
Madhavan Arumugam <madhavanarumugam1111@gmail.com>
2026-05-17 20:39:01 +0530

    Time: 52 ms (83.25%) | Memory: 70.6 MB (28.05%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -7,18 +7,18 @@ class Solution {
 
         for (int right = 0; right < fruits.length; right++) {
 
-            // Add current fruit into basket
+         
             map.put(fruits[right],
                     map.getOrDefault(fruits[right], 0) + 1);
 
-            // If more than 2 fruit types
+          
             while (map.size() > 2) {
 
-                // Remove left fruit count
+            
                 map.put(fruits[left],
                         map.get(fruits[left]) - 1);
 
-                // Remove fruit completely if count becomes 0
+              
                 if (map.get(fruits[left]) == 0) {
                     map.remove(fruits[left]);
                 }
@@ -26,7 +26,7 @@ class Solution {
                 left++;
             }
 
-            // Update maximum window size
+         
             max = Math.max(max, right - left + 1);
         }
 
